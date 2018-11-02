a35526e9f5c8b9daa9b3d9730d1aaa164adae1b1
Anthony Brown <abrown4123@gmail.com>
2018-11-02 18:05:09 -0500

    My solution for finding the max character in a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,17 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+//My Solution
+function maxChar(str) {
+    let hash = {};
+    let max = str[0];
+    for (let char of str) {
+        hash[char] = hash[char] + 1 || 1;
+        if (hash[char] > hash[max]) max = char;
+    }
+    return max;
+}
+
+
 
 module.exports = maxChar;
