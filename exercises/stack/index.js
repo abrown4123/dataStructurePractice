4d1d5a6453173f400a98d38c3d00e7a5acdbef2f
Anthony Brown <abrown4123@gmail.com>
2018-12-09 10:33:05 -0600

    Created a Stack class

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -11,6 +11,21 @@
 //   s.pop(); // returns 1
 
 class Stack {
+    constructor() {
+        this.data = [];
+    }
+
+    push(record) {
+        return this.data.push(record);
+    }
+
+    pop() {
+        return this.data.pop();
+    }
+
+    peek() {
+        return this.data[this.data.length-1];
+    }
 }
 
 module.exports = Stack;
